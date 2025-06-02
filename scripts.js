9e2b3640be9c428b462d795da7fa1f8765f9a694
Aakarshan Jorge <aaka022009@gmail.com>
2025-06-02 21:48:07 +0200

    Project completed

--- scripts.js
+++ scripts.js
@@ -1,3 +1,12 @@
+const weatherContainer = document.querySelector(".weather");
+weatherContainer.style.display = "none";
+const container = document.querySelector(".container");
+
+const errorText = document.createElement("h1");
+errorText.innerHTML = "Failed to find city";
+container.appendChild(errorText);
+errorText.style.display = "none";
+
 async function fetchWeatherData(){
     try{
         const apiKey = "21adf45230c09847f32ae34b8857e8c9";
@@ -7,7 +16,9 @@ async function fetchWeatherData(){
         const windSpeed = document.getElementById("wind");
         const weatherIcon = document.getElementById("weather-icon");
         const city = document.getElementById("city");
-
+        const feelsLike = document.getElementById("feels-like");
+        const highTemp = document.getElementById("high-temp");
+        const lowTemp = document.getElementById("low-temp");
 
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`)
 
@@ -15,11 +26,20 @@ async function fetchWeatherData(){
             throw new Error("Could not find city");
         }
 
+        cityName.value = "";
+
         const data = await response.json();
+        
+        errorText.style.display = "none";
+        weatherContainer.style.display = "block";
         temperature.innerHTML = `${Math.round(data.main.temp)}°C`;
         humidity.innerHTML = `Humidity: ${data.main.humidity}%`;
         windSpeed.innerHTML = `Wind speed: ${Math.round(data.wind.speed)} km/h`;
+        feelsLike.innerHTML = `Feels like: ${Math.round(data.main.feels_like)}°C`;
+        highTemp.innerHTML = `Highest: ${Math.round(data.main.temp_max)}°C`;
+        lowTemp.innerHTML = `Lowest: ${Math.round(data.main.temp_min)}°C`;
         city.innerHTML = data.name;
+        
 
         if(data.weather[0].main === "Clouds"){
             weatherIcon.src = "./images/cloudy.png";
@@ -30,12 +50,12 @@ async function fetchWeatherData(){
         else if(data.weather[0].main === "Rain"){
             weatherIcon.src = "./images/heavy-rain.png";
         }
-        
-        cityName.value = "";
 
         console.log(data);
     }
     catch(error){
         console.error(error);
+        weatherContainer.style.display = "none";
+        errorText.style.display = "block";
     }
 }
